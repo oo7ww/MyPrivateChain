8c0bc34c3656fa167f594732e7d2549aa870e6fc
oo7ww <xlovebear@126.com>
2018-04-25 16:30:25 +0800

    db throw errors

--- account_generator.js
+++ account_generator.js
@@ -2,7 +2,7 @@
 
 //web3 address:port
 var Web3 = require('web3');
-var web3 = new Web3('http://loaclhost:8383');//default port 8545, my port 8381
+var web3 = new Web3('http://loaclhost:8381');//default port 8545, my port 8381
 //account generator
 function Adrsgenerator(x) {
 	if (x <= 0){
@@ -15,8 +15,9 @@ function Adrsgenerator(x) {
 		for(var i = 0; i < x; ++i){
 			var new_account = web3.eth.accounts.create();
 			var address = new_account.address;
-			var privateKey = new_account.privateKey;
+                        var privateKey = new_account.privateKey;
 			accounts[address] = privateKey;
+                        console.log(address + ' ' + privateKey + '\n');
 		}
 		return accounts;
 	}
@@ -26,10 +27,10 @@ function Adrsgenerator(x) {
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 //connection url
-var url = 'mongodb://loaclhost:27017/AccountDB';
+var url = 'mongodb://127.0.0.1:27017/AccountDB';
 
 var num = 100;
-
+var account_file = Adrsgenerator(num);
 MongoClient.connect(url, function(err, db){
 	assert.equal(null, err);
 	console.log("Connect to db server successfully");
@@ -38,7 +39,7 @@ MongoClient.connect(url, function(err, db){
 		if(err) throw err;
 		console.log("collection created");
 	});
-	var account_file = Adrsgenerator(num);
+//	var account_file = Adrsgenerator(num);
 	dbase.collection('AccountDB').insertMany(account_file, function(err, res){
         if (err) throw err;
         console.log("inserted files count: " + res.insertedCount);		
